cd754aee76d4d5339fcdf24ba5d122a7ff9ac75c
Meine Toonen <toonen.meine@gmail.com>
2016-03-01 17:07:47 +0100

    Securityfilter added. used hibernate 3.6.10-final (:(), and added schemageneration

---
+++ src/main/java/nl/b3p/imro/harvester/entities/Gebruiker.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.b3p.imro.harvester.entities;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author Meine Toonen <meinetoonen@b3partners.nl>
+ */
+@Entity
+public class Gebruiker implements Principal{
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    
+    private String username;
+    private String password;
+
+
+    private String passwordsalt;
+    private String passwordhash;
+
+    @ManyToMany
+    @JoinTable(joinColumns=@JoinColumn(name="gebruiker"), inverseJoinColumns=@JoinColumn(name="role"))
+    private Set<Role> roles = new HashSet();
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getPasswordsalt() {
+        return passwordsalt;
+    }
+
+    public void setPasswordsalt(String passwordsalt) {
+        this.passwordsalt = passwordsalt;
+    }
+
+    public String getPasswordhash() {
+        return passwordhash;
+    }
+
+    public void setPasswordhash(String passwordhash) {
+        this.passwordhash = passwordhash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getName() {
+        return getUsername();
+    }    
+}
---
+++ src/main/java/nl/b3p/imro/harvester/entities/Role.java
@@ -0,0 +1,74 @@
+/**
+ * Geo-OV - applicatie voor het registreren van KAR meldpunten
+ *
+ * Copyright (C) 2009-2013 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.b3p.imro.harvester.entities;
+
+import javax.persistence.*;
+import org.stripesstuff.stripersist.Stripersist;
+
+/**
+ * Klasse beschrijft een rol en definieert standaard rollen.
+ *
+ * @author Chris
+ */
+@Entity
+public class Role {
+    /**
+     * beheerder rol
+     */
+    public static final String BEHEERDER = "beheerder";
+    /**
+     * gebruiker rol
+     */
+    public static final String GEBRUIKER = "gebruiker";
+
+    public static final String VERVOERDER = "vervoerder";
+
+    @Id
+    private Integer id;
+
+    private String role;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
