58ad13b694c7521a3d14e1b221b436136b6c2d52
Meine Toonen <toonen.meine@gmail.com>
2016-04-12 14:08:52 +0200

    Changed besluitvak: doesn't inherit ImroEntity anymore, but own properties. Now has besluitgebied as property (instead of Bestemmingsplan)

--- imro-persistence/src/main/java/nl/b3p/imro/harvester/entities/imro/Besluitvlak.java
+++ imro-persistence/src/main/java/nl/b3p/imro/harvester/entities/imro/Besluitvlak.java
@@ -16,13 +16,103 @@
  */
 package nl.b3p.imro.harvester.entities.imro;
 
+import com.vividsolutions.jts.geom.MultiPolygon;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  *
  * @author Meine Toonen <meinetoonen@b3partners.nl>
  */
 @Entity
-public class Besluitvlak extends ImroEntity{
+public class Besluitvlak {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String identificatie;
+
+    private String naam;
+
+    @org.hibernate.annotations.Type(type = "org.hibernatespatial.GeometryUserType")
+    private MultiPolygon geometrie;
+
+    private String artikelnummer;
+
+    private String verwijzing;
+
+    private String typePlanObject;
+
+    @ManyToOne
+    private Besluitgebied besluitgebied;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIdentificatie() {
+        return identificatie;
+    }
+
+    public void setIdentificatie(String identificatie) {
+        this.identificatie = identificatie;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public MultiPolygon getGeometrie() {
+        return geometrie;
+    }
+
+    public void setGeometrie(MultiPolygon geometrie) {
+        this.geometrie = geometrie;
+    }
+
+    public String getArtikelnummer() {
+        return artikelnummer;
+    }
+
+    public void setArtikelnummer(String artikelnummer) {
+        this.artikelnummer = artikelnummer;
+    }
+
+    public String getVerwijzing() {
+        return verwijzing;
+    }
+
+    public void setVerwijzing(String verwijzing) {
+        this.verwijzing = verwijzing;
+    }
+
+    public String getTypePlanObject() {
+        return typePlanObject;
+    }
+
+    public void setTypePlanObject(String typePlanObject) {
+        this.typePlanObject = typePlanObject;
+    }
+
+    public Besluitgebied getBesluitgebied() {
+        return besluitgebied;
+    }
+
+    public void setBesluitgebied(Besluitgebied besluitgebied) {
+        this.besluitgebied = besluitgebied;
+    }
+
 
 }
